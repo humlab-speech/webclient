005992d9b1c026ba996bf571bb23e829438097f1
root <root@humlab-visp-demo.srv.its.umu.se>
2022-08-29 16:20:44 +0200

    visp-demo prod build environment

---
+++ src/environments/environment.visp-demo.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    APPLICATION_NAME: "Visible Speech Demo",
+    LOGO_IMAGE_PATH: "/assets/VISP-logo-white.png",
+    API_ENDPOINT: "https://visp-demo.humlab.umu.se",
+    EMUDB_INTEGRATION: true,
+    ENABLED_APPLICATIONS: ["rstudio", "jupyter", "emu-webapp", "octra", "script"],
+    BASE_DOMAIN: "visp-demo.humlab.umu.se",
+    GITLAB_API_ENDPOINT: "https://gitlab.visp-demo.humlab.umu.se/api/v4",
+    GITLAB_PERSONAL_ACCESS_TOKEN: null,
+    ACCESS_LIST_ENABLED: false,
+    production: true
+  };
