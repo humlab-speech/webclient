796cc04f557fb41d1f520b8ce92fcef062a7fb6a
Johan von Boer <johan.von.boer@umu.se>
2026-03-17 11:01:14 +0100

    Fixed an issue with testuser login

--- src/index.php
+++ src/index.php
@@ -76,35 +76,37 @@ if($shibHeadersFound) {
 $autoCreate = false;
 
 if(!empty(getenv("TEST_USER_LOGIN_KEY")) && $_GET['login'] == getenv("TEST_USER_LOGIN_KEY")) {
-  if($_GET['user'] == "test2") {
-    addLog("Starting session for testuser2@example.com", "info");
-    $_SESSION['firstName'] = "Test2";
-    $_SESSION['lastName'] = "User2";
-    $_SESSION['fullName'] = "Test2 User2";
-    $_SESSION['email'] = "testuser2@example.com";
-    $_SESSION['eppn'] = "testuser2@example.com";
-    $_SESSION['username'] = formatEppn($_SESSION['eppn']);
-    $_SESSION['testUser'] = true;
-  }
-  else if($_GET['user'] == "test3") {
-    addLog("Starting session for testuser3@example.com", "info");
-    $_SESSION['firstName'] = "Test3";
-    $_SESSION['lastName'] = "User3";
-    $_SESSION['fullName'] = "Test3 User3";
-    $_SESSION['email'] = "testuser3@example.com";
-    $_SESSION['eppn'] = "testuser3@example.com";
-    $_SESSION['username'] = formatEppn($_SESSION['eppn']);
-    $_SESSION['testUser'] = true;
-  }
-  else if($_GET['user'] == "test4") {
-    addLog("Starting session for testuser4@example.com", "info");
-    $_SESSION['firstName'] = "Test4";
-    $_SESSION['lastName'] = "User4";
-    $_SESSION['fullName'] = "Test4 User4";
-    $_SESSION['email'] = "testuser4@example.com";
-    $_SESSION['eppn'] = "testuser4@example.com";
-    $_SESSION['username'] = formatEppn($_SESSION['eppn']);
-    $_SESSION['testUser'] = true;
+  if(isset($_GET['user'])) {
+    if($_GET['user'] == "test2") {
+      addLog("Starting session for testuser2@example.com", "info");
+      $_SESSION['firstName'] = "Test2";
+      $_SESSION['lastName'] = "User2";
+      $_SESSION['fullName'] = "Test2 User2";
+      $_SESSION['email'] = "testuser2@example.com";
+      $_SESSION['eppn'] = "testuser2@example.com";
+      $_SESSION['username'] = formatEppn($_SESSION['eppn']);
+      $_SESSION['testUser'] = true;
+    }
+    else if($_GET['user'] == "test3") {
+      addLog("Starting session for testuser3@example.com", "info");
+      $_SESSION['firstName'] = "Test3";
+      $_SESSION['lastName'] = "User3";
+      $_SESSION['fullName'] = "Test3 User3";
+      $_SESSION['email'] = "testuser3@example.com";
+      $_SESSION['eppn'] = "testuser3@example.com";
+      $_SESSION['username'] = formatEppn($_SESSION['eppn']);
+      $_SESSION['testUser'] = true;
+    }
+    else if($_GET['user'] == "test4") {
+      addLog("Starting session for testuser4@example.com", "info");
+      $_SESSION['firstName'] = "Test4";
+      $_SESSION['lastName'] = "User4";
+      $_SESSION['fullName'] = "Test4 User4";
+      $_SESSION['email'] = "testuser4@example.com";
+      $_SESSION['eppn'] = "testuser4@example.com";
+      $_SESSION['username'] = formatEppn($_SESSION['eppn']);
+      $_SESSION['testUser'] = true;
+    }
   }
   else {
     addLog("Starting session for testuser@example.com", "info");
@@ -118,9 +120,7 @@ if(!empty(getenv("TEST_USER_LOGIN_KEY")) && $_GET['login'] == getenv("TEST_USER_
     $autoCreate = true;
   }
 }
-else {
-  addLog("Not starting session for any testuser", "info");
-}
+
 
 addLog("Started session ".$sid."", "info");
 
