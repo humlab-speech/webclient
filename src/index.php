33435891b173c34954530f938a96ec83583b4d51
Tomas Skotare <tomas.skotare@umu.se>
2026-04-08 16:22:46 +0200

    feat(matomo): add tracker script tag to index.php
    Load /matomo-tracker.js with defer attribute. The script is served by
    Apache from a generated file (not part of the webclient build).

--- src/index.php
+++ src/index.php
@@ -205,6 +205,7 @@ if(!empty($_SESSION['username'])) {
       shibIdentityProvider: "<?php echo $_SESSION['shibIdentityProvider']; ?>",
     };
   </script>
+  <script src="/matomo-tracker.js" defer></script>
 </head>
 <body>
   <app-root></app-root>
